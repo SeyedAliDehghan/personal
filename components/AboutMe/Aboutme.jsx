4d8128107109957fa45edb976d6a8d213c749f6c
Seyed Ali Dehghan <alidehghan04@gmail.com>
2022-07-28 17:19:06 +0430

    about me page and component created

--- components/AboutMe/Aboutme.jsx
+++ components/AboutMe/Aboutme.jsx
@@ -1,50 +1,76 @@
-import Image from "next/image";
-import logo from "../../public/img/logo.png";
-import me from "../../public/img/me.jpg";
-import Link from "next/link";
 
-export default function Aboutme() {
+import Link from "next/link";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Aboutme({ technologies }) {
+  const router = useRouter();
+
   // const logoUrl = "/img/"+logoName;
   return (
-    <div className="mt-20 md:mt-56 pt-20 container mx-auto px-6">
-      <h2 className="sectionTitle flex flex-col md:flex-row md:items-end mb-8 relative">
-        <span>
-          <span className="text-primaryColor mr-2">01.</span>
-          <span className="text-4xl text-secondaryColor font-sans">
-            About Me
-          </span>
-        </span>
-      </h2>
-      <div className="flex flex-col md:flex-row items-center md:space-y-0">
-        <div className="flex flex-col  space-y-12 md:w-1/2">
+    <div className="container flex flex-col px-4 mx-auto mt-10 space-y-12">
+      {router.pathname === "/" && (
+        <Link href="/aboutme/">
+          <a>
+            <div className="flex justify-between">
+              <h2 className="text-4xl font-bold text-center md:text-left mb-5">
+                About Me
+              </h2>
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                style={{ width: "15px", marginRight: "5px" }}
+              />
+            </div>
+          </a>
+        </Link>
+      )}
+      {router.pathname.includes("/aboutme") && (
+        <Link href="/aboutme/">
+          <a>
+            <h1 className="text-4xl font-bold text-center md:text-left mb-5">
+              About Me
+            </h1>
+          </a>
+        </Link>
+      )}
+      <div className="flex flex-col md:flex-row items-center mt-10">
+        <div className="flex flex-col  space-y-12 w-full">
           <p className="text-mainTextColor font-sans">
-            Hello! My name is Abolfazl and I enjoy creating things that live on
-            the internet. My interest in web development started back in 2018
-            when I decided to try editing custom WordPress themes — turns out
-            hacking together a custom button taught me a lot about HTML & CSS!
+            Hello! My name is Seyed Ali Dehghan and I enjoy creating things that
+            live on the internet.
           </p>
-          <p className="text-mainTextColor font-sans">
-            Here are a few technologies I’ve been working with recently:
-          </p>
-          <ul className="text-mainTextColor aboutMeList">
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-            <li className="flex items-center">JavaScript</li>
-          </ul>
+          {technologies.length!==0 && (
+            <>
+              <p className="text-mainTextColor font-sans">
+                Here are a few technologies I’ve been working with recently:
+              </p>
+              <ul className="text-mainTextColor aboutMeList">
+                {technologies.map((technology) => (
+                  <li className="flex items-center" key={technology._id}>
+                    <FontAwesomeIcon
+                      icon={faAngleRight}
+                      style={{ width: "7px", marginRight: "7px" }}
+                    />
+                    <span>{technology.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
-        <div className="md:w-1/2 flex justify-center rounded">
+        {/* <div className="md:w-1/2 flex justify-center rounded">
           <div className="myImgContainer ">
             <div className=" myImgItem rounded float-right	">
               <img src="/img/me.jpg" alt="" className="rounded myImg" />
-              {/* <Image src={me} alt="Seyed Ali Dehghan" className="rounded meImg"/> */}
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
+
+
+
