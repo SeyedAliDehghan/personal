147d94bad093ae6a3245e6bdf159b422fb3b9fce
Seyed Ali Dehghan <alidehghan04@gmail.com>
2022-07-31 21:02:17 +0430

    admin panel get child

--- components/Panel/Panel.jsx
+++ components/Panel/Panel.jsx
@@ -3,8 +3,12 @@ import illustration from "../../public/img/illustration.jpg";
 import Parallax from "parallax-js";
 import React, { useRef, useEffect } from "react";
 import Socialmedias from "../Socialmedias/Socialmedias";
-
-export default function Panel({ children}) {
+import { useSession } from "next-auth/react";
+import List from "./list/list";
+export default function Panel({ children,activeItem}) {
+  const { data: session } = useSession();
+// console.log(activeItem)
+  // console.log(session)
 
   return (
     <div className="h-screen flex overflow-hidden bg-slate-200">
@@ -21,60 +25,14 @@ export default function Panel({ children}) {
       </div>
       {/* menu items */}
       <div className="px-4 py-2">
-        <ul>
-          <li>
-            <a
-              href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
-            >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
-            >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
-            >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-blue-600 hover:text-blue-600 hover:bg-gray-200 bg-gray-200"
-            >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Active</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
-            >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
-            </a>
-          </li>
-        </ul>
+        <List activeItem={activeItem}/>
       </div>
     </div>
     {/* content */}
     <div className="flex-1 flex-col relative z-0 overflow-y-auto">
       {/* head */}
       <div className="px-4 md:px-8 py-2 h-16 flex justify-between items-center shadow-sm bg-white">
-        <div>item</div>
+        <div>{session.user.name}</div>
         <div>item</div>
       </div>
       <div className="md:max-w-6xl md:mx-auto px-4 py-8">
