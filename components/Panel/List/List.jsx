cd473f69bf2800c8f4f4fd5dc49bffb002408251
Seyed Ali Dehghan <alidehghan04@gmail.com>
2022-08-01 17:17:10 +0430

    add more items in list

--- components/Panel/List/List.jsx
+++ components/Panel/List/List.jsx
@@ -1,5 +1,7 @@
 import { useSession } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaste } from "@fortawesome/free-regular-svg-icons";
+
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default function List({ activeItem}) {
@@ -14,10 +16,12 @@ export default function List({ activeItem}) {
               href="#"
               className={`mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200 ${activeItem==='home'?' bg-gray-200 text-blue-600':null}`}
             >
-              <span className="mr-4 opacity-50"><FontAwesomeIcon
+              <span className="mr-4 opacity-50">
+                <FontAwesomeIcon
                 icon={faHome}
                 style={{ width: "15px", marginRight: "5px" }}
-              /></span>
+              />
+              </span>
               
               <span>Dashboard</span>
             </a>
@@ -25,19 +29,24 @@ export default function List({ activeItem}) {
           <li>
             <a
               href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
+              className={`mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200 ${activeItem==='projects'?' bg-gray-200 text-blue-600':null}`}
             >
-              <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
+              <span className="mr-4 opacity-50">
+              <FontAwesomeIcon
+                icon={faPaste}
+                style={{ width: "15px", marginRight: "5px" }}
+              />
+              </span>
+              <span>Projects</span>
             </a>
           </li>
           <li>
             <a
               href="#"
-              className="mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200"
+              className={`mb-1 px-2 py-2 rounded-lg flex items-center font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-200 ${activeItem==='posts'?' bg-gray-200 text-blue-600':null}`}
             >
               <span className="mr-4 opacity-50">♥</span>
-              <span>Item</span>
+              <span>Posts</span>
             </a>
           </li>
           <li>
