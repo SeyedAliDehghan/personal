6f45868237f190986aaaf18391427a571c372000
Seyed Ali Dehghan <alidehghan04@gmail.com>
2022-08-01 10:43:31 +0430

    add and delete technology added

--- pages/api/admin/technologies/index.js
+++ pages/api/admin/technologies/index.js
@@ -4,7 +4,6 @@ import Setting from "../../../../models/setting";
 export default async function handler(req, res) {
   const { method, cookies } = req;
   dbConnect();
-
   if (method === "GET") {
     try {
       const settings = await Setting.find({});
@@ -12,24 +11,29 @@ export default async function handler(req, res) {
 
       res.send(setting.technologies);
     } catch (e) {
+      res.status(500).send({ error: e.message });
+    }
+  }
+  if (method === "POST") {
+    try {
+      const settings = await Setting.findOne({});
+      await settings.technologies.push(req.body);
+      await settings.save();
+      res.send(settings.technologies);
+    } catch (e) {
       console.log(e);
       res.status(500).send({ error: e.message });
     }
-    if (method === "POST") {
-      console.log("asd");
-
-      try {
-        let settings = await Setting.find({});
-        settings = settings[0].technologies({
-          ...settings[0].technologies,
-          ...req.body,
-        });
-        await settings.save();
-        res.send({"e":"w"});
-      } catch (e) {
-        // console.log(e);
-        res.status(500).send({ error: e.message });
-      }
+  }
+  if (method === "DELETE") {
+    try {
+      const settings = await Setting.findOne({});
+      settings.technologies=await settings.technologies.filter(tech=>tech._id!=req.body.id);
+      await settings.save();
+      res.send(settings.technologies);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send({ error: e.message });
     }
   }
 }
