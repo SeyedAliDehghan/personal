c30013b8a05cb835e4f296c1f505a7df6a796def
Seyed Ali Dehghan <alidehghan04@gmail.com>
2022-09-05 12:14:18 +0430

    new card for project items

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -25,7 +25,7 @@ export default function Projects({ projects }) {
 
   return (
     <div className="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0">
-      {router.pathname===("/") && (
+      {router.pathname === "/" && (
         <Link href="/projects/">
           <a>
             <div className="flex justify-between">
@@ -43,63 +43,61 @@ export default function Projects({ projects }) {
       {router.pathname.includes("/projects") && (
         <Link href="/projects/">
           <a>
-              <h1 className="text-4xl font-bold text-center md:text-left mb-5">
-                Projects
-              </h1>
+            <h1 className="text-4xl font-bold text-center md:text-left mb-5">
+              Projects
+            </h1>
           </a>
         </Link>
       )}
-      { project.length!==0 && (
+      {project.length !== 0 && (
         <div className="flex flex-wrap">
-        {project.map((project) => (
-          <div className="p-4 w-full sm:w-1/2 lg:w-1/3 relative" key={project._id}>
-            {project.tag==="mini" && (
-              <div className="ribbon ribbon-top-left z-20"><span>{project.tag}</span></div>
-              // <div className="absolute z-30 top-7 px-7 py-1 bg-primaryColor text-white rounded-r-md labelShaddow font-bold">Mini</div>
-            )}
-            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-              <div className="projectImageCcontainer">
-                <Image
-                  src="/img/mock.jpg"
-                  alt={project.title}
-                  layout="fill"
-                  className="projectImage"
-                />
+          {project.map((project) => (
+            <div
+              className="p-4 w-full sm:w-1/2 lg:w-1/3 relative  rounded-lg"
+              key={project._id}
+            >
+              {project.tag === "mini" && (
+                <div className="ribbon ribbon-top-left z-20">
+                  <span>{project.tag}</span>
+                </div>
+                // <div className="absolute z-30 top-7 px-7 py-1 bg-primaryColor text-white rounded-r-md labelShaddow font-bold">Mini</div>
+              )}
+              {/* h-full */}
+              <div className=" border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <div className="projectImageCcontainer">
+                  <Image
+                    src="/img/mock.jpg"
+                    alt={project.title}
+                    layout="fill"
+                    className="projectImage"
+                  />
+                </div>
               </div>
-              <div className="p-6 transition duration-300 ease-in">
-                <div className="text-base font-medium text-primaryColor mb-1">
-                  {project.publicDate}
-                </div>
-                <div className="text-2xl font-semibold mb-3">
-                  {project.title}
-                </div>
-                <div className="leading-relaxed mb-3">
-                  {project.description}
-                </div>
-                <div className="flex items-center flex-wrap ">
-                  <Link href={"/projects/" + project.slug}>
-                    <a className="text-primaryColor inline-flex items-center md:mb-2 lg:mb-0">
-                      Read More
-                    </a>
-                  </Link>
-                  <div className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                    {/* <FontAwesomeIcon icon={faGithub} style={{width:"15px",marginRight:"5px"}}/> */}
+              <div className="px-4 relative  -mt-16">
+                <div className="bg-white rounded-lg px-4 py-3 shadow-lg">
+                  <span className="bg-orange-200 text-orange-900 font-medium rounded-full px-2 inline-block uppercase tracking-wide text-xs">
+                    {project.publicDate}
+                  </span>
+                  <h2 className="mt-2 text-gray-900 font-semibold text-lg">
+                    {project.title}
+                  </h2>
+                  <div className="mt-2 text-sm text-gray-600">
+                    {project.description}
                   </div>
-                  {/* <div className="text-gray-400 inline-flex items-center leading-none text-sm">
-                    <FontAwesomeIcon
-                      icon={faMessage}
-                      style={{ width: "15px", marginRight: "5px" }}
-                    />
-                    <span>{project.commentCount}</span>
-                  </div> */}
+                  <div className="mt-3">
+                    <Link href={"/projects/" + project.slug}>
+                      <a className="text-primaryColor rounded-lg">
+                        Read More
+                      </a>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       )}
-      {project.length===0 && (<p>No project published yet</p>)}
+      {project.length === 0 && <p>No project published yet</p>}
     </div>
   );
 }
